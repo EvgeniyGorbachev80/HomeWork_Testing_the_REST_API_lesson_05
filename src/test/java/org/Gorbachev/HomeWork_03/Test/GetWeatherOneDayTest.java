a9cf623b61c3360cedba612c90da8a68b1e42adb
Evgeniy <evgeniy.gorbachev80@gmail.com>
2024-04-27 17:11:28 +0300

    add

--- src/test/java/org/Gorbachev/HomeWork_03/Test/GetWeatherOneDayTest.java
+++ src/test/java/org/Gorbachev/HomeWork_03/Test/GetWeatherOneDayTest.java
@@ -1,8 +1,11 @@
 package org.Gorbachev.HomeWork_03.Test;
 
+import io.qameta.allure.*;
+import jdk.jfr.Description;
 import org.Gorbachev.HomeWork_03.accuweather.weather.DailyForecast;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.Gorbachev.HomeWork_03.accuweather.weather.Weather;
 
@@ -10,8 +13,16 @@ import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
+@Epic(value = "Тестирование API http://dataservice.accuweather.com")
+@Feature(value = "Домашнее задание")
 public class GetWeatherOneDayTest extends AccuweatherAbstractTest {
     @Test
+    @DisplayName("GetWeatherOneDayTest")
+    @Description("GET WeatherOneDay")
+    @Link("")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Evgeniy Gorbachev")
+    @Story(value = "Тестирование метода getWeatherOneDay_shouldReturn")
     void getWeatherOneDay_shouldReturn() {
 
         Weather response = given()
@@ -30,6 +41,12 @@ public class GetWeatherOneDayTest extends AccuweatherAbstractTest {
     }
 
     @Test
+    @DisplayName("GetWeatherOneDayTest")
+    @Description("GET DailyForecastsList")
+    @Link("")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Evgeniy Gorbachev")
+    @Story(value = "Тестирование метода getDailyForecastsList")
     void getDailyForecastsList() {
 
         List<DailyForecast> response = given()
@@ -47,6 +64,12 @@ public class GetWeatherOneDayTest extends AccuweatherAbstractTest {
     }
 
     @Test
+    @DisplayName("GetWeatherOneDayTest")
+    @Description("GET String")
+    @Link("")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Evgeniy Gorbachev")
+    @Story(value = "Тестирование метода getString")
     void getString() {
         String response = given()
                 .queryParam("apikey", getApiKey())
